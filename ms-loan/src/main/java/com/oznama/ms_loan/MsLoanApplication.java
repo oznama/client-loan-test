a1ad57c39c16a8c221b73736779b001be69ae48e
Oziel Naranjo <oznama27@gmail.com>
2026-06-11 23:52:24 -0600

    Initial structure

---
+++ ms-loan/src/main/java/com/oznama/ms_loan/MsLoanApplication.java
@@ -0,0 +1,13 @@
+package com.oznama.ms_loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLoanApplication.class, args);
+	}
+
+}
